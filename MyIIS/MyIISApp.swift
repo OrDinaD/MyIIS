543d7771097e0c2a421e679b8a033a3ba00cd22a
OrDinaD <Vlad.vasilevskiy.07@gmail.com>
2025-10-13 12:12:17 +0300

    Initial Commit

---
+++ MyIIS/MyIISApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIISApp.swift
+//  MyIIS
+//
+//  Created by Влад on 13.10.25.
+//
+
+import SwiftUI
+
+@main
+struct MyIISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
